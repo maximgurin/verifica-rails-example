9371461005e3da09d42565916551337898480d70
Maxim Gurin <mg@maximgurin.com>
2023-01-29 13:03:43 +0100

    Fix N+1 to help readers to focus on what matters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def current_user
-    cookies[:user_id].try { |id| User.find_by(id:) }
+    cookies[:user_id].try { |id| User.includes(:organization).find_by(id:) }
   end
 end
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,13 +5,17 @@ class MainController < ApplicationController
       return redirect_to login_path(user_id: superuser.id)
     end
 
-    @users = User.all
+    @users = User.all.includes(:organization)
     @current_user = current_user
     @total_videos = Video.count
 
+    available_videos = Video
+      .includes(:distribution_setting, author: [:organization])
+      .available_for(current_user)
+      .order(:name)
+
     # add allowed actions to each video so view can decide which buttons to render
-    # naive implementation, N+1 problem in VideoAclProvider, don't copy this to a real project
-    @videos = Video.available_for(current_user).order(:name).each do |video|
+    @videos = available_videos.each do |video|
       video.allowed_actions = AUTHORIZER.allowed_actions(current_user, video)
     end
   end
